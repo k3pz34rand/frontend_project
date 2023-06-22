6d267e289708abb70857a4a6c8c8d769f8d5865f
pismalx <pismalx@gmail.com>
2023-06-22 23:21:17 +0300

    configuration jest and add classnames test

---
+++ config/jest/jest.config.ts
@@ -0,0 +1,11 @@
+const config = {
+    clearMocks: true,
+    testEnvironment: 'jsdom',
+    coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+    moduleDirectories: ['node_modules'],
+    testMatch: ['<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'],
+    rootDir: '../../',
+};
+
+export default config;
