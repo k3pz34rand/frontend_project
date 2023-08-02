efc7fe3ed7aa902862da9cf98f0de06ccdcc066b
pismalx <pismalx@gmail.com>
2023-08-02 23:27:53 +0300

    add Login tests

--- src/features/AuthByUserName/model/slice/loginSlice.test.ts
+++ src/features/AuthByUserName/model/slice/loginSlice.test.ts
@@ -8,7 +8,7 @@ describe('loginSlice.test', () => {
         expect(
             loginReducer(
                 state as LoginSchema,
-                loginActions.setUsername('123123')
+                loginActions.setUserName('123123')
             )
         ).toEqual({ username: '123123' });
     });
